1d8eb2e7b1c05915afc7479263fd3e1d757a57d9
Salome Ranson <salomeranson@gmail.com>
2021-05-22 11:15:32 -0400

    updated dashboard route to go to dashboard-uploadButton handlebar screen

--- routes/dashboard-routes.js
+++ routes/dashboard-routes.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 
 
 router.get("/", (req, res) => {
-    res.render("dashboard-submitUpload");
+    res.render("dashboard-uploadButton");
 });
 
 module.exports = router;
