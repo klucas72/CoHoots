8c80ea5ef40858b7b7b9cf42be6ca57c8f6dfb83
Karen Roh <karenroh@hotmail.com>
2021-05-24 17:14:13 -0400

    successfully hashing password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -19,4 +19,44 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    const userInfo = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+
+    if (!userInfo) {
+      res
+        .status(400)
+        .json({ message: "Incorrect username or password. Please try again!" });
+      return;
+    }
+
+    // Checks if the password matches the bcrypted password in User model
+    const validPassword = await userInfo.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect password. Please try again!" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      req.session.userId = userInfo.id;
+      req.session.username = userInfo.username;
+
+      res
+        .status(200)
+        .json({ user: userInfo, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
