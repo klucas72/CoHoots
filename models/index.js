8011bbf7f8aeac34a0fa2e93825c3756baa58817
Karen Roh <karenroh@hotmail.com>
2021-05-24 15:39:09 -0400

    fixed Cyclic dependency error by getting rid of image model and its associations

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 // import models
 const Comment = require("./Comment");
 const Design = require("./Design");
-const Image = require("./Image");
+// const Image = require("./Image");
 const Like = require("./Like");
 const User = require("./User");
 
@@ -24,10 +24,10 @@ Like.belongsTo(User, {
 });
 
 // each design has one image
-Design.hasOne(Image, {
-  foreignKey: "imageId",
-  onDelete: "CASCADE",
-});
+// Design.hasOne(Image, {
+//   foreignKey: "imageId",
+//   onDelete: "CASCADE",
+// });
 
 // a design can have many likes through the designId foreign key on model: Like, key: designId
 Design.hasMany(Like, {
@@ -41,7 +41,7 @@ Design.hasMany(Comment, {
 module.exports = {
   Comment,
   Design,
-  Image,
+  // Image,
   Like,
   User,
 };
