938a186fe43a610bd3f95011686860b5216a9d50
Karen Roh <karenroh@hotmail.com>
2021-05-24 15:02:00 -0400

    created signup.js and trying to create a new user in user-routes.js

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,3 +1,22 @@
 const router = require("express").Router();
+const { User } = require("../../models");
+
+// Create new user
+router.post("/", async (req, res) => {
+  try {
+    const userInfo = await User.create(req.body);
+
+    // Saving user into a session
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      req.session.userId = userInfo.id;
+      req.session.username = userInfo.username;
+
+      res.json(userInfo);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- routes/home-routes.js
+++ routes/home-routes.js
@@ -2,27 +2,27 @@ const router = require("express").Router();
 
 // Homepage route
 router.get("/", (req, res) => {
-    res.render("all-designs");
+  res.render("all-designs");
 });
 
 // Login route
 router.get("/login", (req, res) => {
-    res.render("login");
-})
+  res.render("login");
+});
 
 // Sign Up route
 router.get("/signup", (req, res) => {
-    res.render("signup");
-})
+  res.render("signup");
+});
 
-router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
-        req.session.destroy(() => {
-            res.status(204).end();
-        });
-    } else {
-        res.status(404).end();
-    }
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
