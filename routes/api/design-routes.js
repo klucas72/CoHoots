a3df6556ed2c274df1d72ed59eff956991669f0d
Salome Ranson <salomeranson@gmail.com>
2021-05-26 10:41:22 -0400

    completed route to get all design from database

--- routes/api/design-routes.js
+++ routes/api/design-routes.js
@@ -2,6 +2,9 @@ const router = require("express").Router();
 const { Design, User } = require('../../models');
 // const ensureAuthenticated = require('../../utils/auth');
 
+
+
+// Create a new design
 router.post("/new", async (req, res) => {
   try {
     const designInfo = await Design.create({
@@ -15,36 +18,20 @@ router.post("/new", async (req, res) => {
   }
 });
 
-// router.post('/', async (req, res) => {
-//     try {
-//       console.log(req.body);
-//       const newDesign = await Design.create({
-//         image_url: req.body.image_url,
-//         user_id: req.body.user_id,
-//         price: req.body.price
-//         // user_id: req.session.userId,
-//       });
-  
-//       res.status(200).json(newDesign);
-//       res.send('GOT THE DESIGN DATA!');
-//     } catch (err) {
-//       res.status(400).json(err);
-//     }
-//   });
 
 
 // Create a route to GET ALL designs
-// router.get('/', async (req, res) => {
-//     try {
-//       const designData = await Design.findAll();
+router.get('/', async (req, res) => {
+    try {
+      const designData = await Design.findAll();
   
-//       const designs = designData.map((design) => design.get({ plain: true }));
-  
-//       res.render('', { posts });
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
+      const designs = designData.map((design) => design.get({ plain: true }));
+      res.json(designs);
+      // res.render('', { designs });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 
 
