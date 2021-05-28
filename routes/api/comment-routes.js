033fab974bb614f5b42f0957a5e326138f97da88
klucas72 <klucas72@hotmail.com>
2021-05-27 20:54:57 -0400

    added comment routes

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Comment } = require('../../models');
+const { Comment, User } = require('../../models');
 const ensureAuthenticated = require('../../utils/auth');
 
 router.post('/', async (req, res) => {
@@ -17,4 +17,50 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.get("/", async (req, res) => {
+    console.log(req.body);
+    try {
+        const commentData = await Comment.findAll({
+            where: {
+                // user_id: req.session.userId,
+                design_id: req.body,
+            },
+        });
+        const comments = commentData.map((comment) => comment.get({ plain: true }));
+        res.json(comments);
+        // res.json(designs);
+    } catch (err) {
+        res.status(500);
+    }
+});
+// router.get('/:id', async (req, res) => {
+//     try {
+//         const allComments = await Comment.findAll({
+//             include: [User],
+//             where: {
+//                 design_id: req.body.design_id
+//             }
+//         })
+//         // const comments = postComments.map((Comment) => Comment.get({ plain: true }));
+
+//         res.render('dashboard-withComments')
+//     } catch (err) {
+//         res.status(404).end();
+//     }
+// })
+
+// router.get("/:id", (req, res) => {
+//     console.log(req.body);
+//     Comment.findAll({
+//         where: {
+//             design_id: req.params.design_id,
+//         },
+//     }).then((design) => {
+//         res.json(design);
+//         // res.render('');
+//     })
+//         .catch((err) => {
+//             res.status(400).json(err);
+//         });
+// });
 module.exports = router;
